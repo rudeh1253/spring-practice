f070978b6227dcef5e8920a4635265ac7330fe34
PGD <rudeh1253@gmail.com>
2023-06-11 19:27:07 +0900

    Add a project for Spring REST practice

---
+++ rest-prac/src/main/java/com/nsl/prac/RestPracApplication.java
@@ -0,0 +1,13 @@
+package com.nsl.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPracApplication.class, args);
+	}
+
+}
