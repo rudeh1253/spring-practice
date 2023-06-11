85ffc8e44ab0c976ccbf6362f377c214d7b28660
PGD <rudeh1253@gmail.com>
2023-06-11 23:02:51 +0900

    Add new project

---
+++ vertex-rest-prac/src/main/java/com/nsl/prac/VertexRestPracApplication.java
@@ -0,0 +1,13 @@
+package com.nsl.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertexRestPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertexRestPracApplication.class, args);
+	}
+
+}
---
+++ vertex-rest-prac/src/test/java/com/nsl/prac/VertexRestPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsl.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertexRestPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
