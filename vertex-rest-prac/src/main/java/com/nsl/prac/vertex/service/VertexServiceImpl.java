df343010aa8bd45d23efba9d0ad9989f75af0c68
PGD <rudeh1253@gmail.com>
2023-06-13 23:45:05 +0900

    Add @Transactional to the service class

--- vertex-rest-prac/src/main/java/com/nsl/prac/vertex/service/VertexServiceImpl.java
+++ vertex-rest-prac/src/main/java/com/nsl/prac/vertex/service/VertexServiceImpl.java
@@ -5,6 +5,7 @@ import com.nsl.prac.vertex.dto.VertexResponseDTO;
 import com.nsl.prac.vertex.repository.VertexRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,7 @@ public class VertexServiceImpl implements VertexService {
     VertexRepository vertexRepository;
 
     @Override
+    @Transactional
     public VertexResponseDTO findById(int id) {
         Optional<Vertex> result = vertexRepository.findById(id);
         VertexResponseDTO dto = VertexResponseDTO.from(result.orElse(null));
@@ -23,6 +25,7 @@ public class VertexServiceImpl implements VertexService {
     }
 
     @Override
+    @Transactional
     public List<VertexResponseDTO> findAll() {
         return vertexRepository.findAll().stream()
                 .map(VertexResponseDTO::from)
@@ -30,21 +33,25 @@ public class VertexServiceImpl implements VertexService {
     }
 
     @Override
+    @Transactional
     public void save(Vertex vertex) {
         vertexRepository.save(vertex);
     }
 
     @Override
+    @Transactional
     public void update(int id, String title) {
         vertexRepository.update(id, title);
     }
 
     @Override
+    @Transactional
     public void update(int id, float xPos, float yPos) {
         vertexRepository.update(id, xPos, yPos);
     }
 
     @Override
+    @Transactional
     public void deleteById(int id) {
         vertexRepository.deleteById(id);
     }
