63a0e0d5a41f283682ae848ad4af2dfa30b9195f
pgd <rudeh1253@gmail.com>
2023-06-22 17:13:06 +0900

    MVC - implement form

---
+++ kyh-spring/5 - web-mvc-example/src/main/java/hello/controller/HomeController.java
@@ -0,0 +1,36 @@
+package hello.controller;
+
+import hello.domain.Member;
+import hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+    MemberService memberService;
+
+    @Autowired
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
---
+++ kyh-spring/5 - web-mvc-example/src/main/java/hello/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.service;
+
+import hello.domain.Member;
+import hello.repository.MemberRepository;
+import hello.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        validateDuplicateOfName(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+    
+    private void validateDuplicateOfName(Member member) {
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent((m) -> {
+            throw new IllegalStateException("이미 존재하는 이름입니다.");
+        });
+    }
+    
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
