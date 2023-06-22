112e9e9f58bee04155472035042bc864db034133
pgd <rudeh1253@gmail.com>
2023-06-22 16:50:53 +0900

    Component management by Java code using @Configuration

---
+++ kyh-spring/4 - spring-bean-dependency-relationship/src/main/java/hello/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello;
+
+import hello.repository.MemberRepository;
+import hello.repository.MemoryMemberRepository;
+import hello.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
