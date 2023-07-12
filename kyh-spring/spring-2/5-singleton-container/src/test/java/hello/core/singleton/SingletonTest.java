ff59976eb729e1f3aeaaedccb1f2f22b28cc0af9
pgd <rudeh1253@gmail.com>
2023-07-12 15:28:35 +0900

    spring singleton container

--- kyh-spring/spring-2/5-singleton-container/src/test/java/hello/core/singleton/SingletonTest.java
+++ kyh-spring/spring-2/5-singleton-container/src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,7 @@ import hello.core.member.MemberService;
 import hello.core.order.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,5 +31,18 @@ public class SingletonTest {
 
         // assertSame: 주소값 비교
         assertSame(singletonService1, singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        // 2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertSame(memberService2, memberService1);
     }
 }
